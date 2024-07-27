38b62d16659822d2eb6f838f1f225baf4ef8485b
Bamireid0 <166797021+Bamireid0@users.noreply.github.com>
2024-07-27 04:39:36 +0100

    Update config.js

--- config.js
+++ config.js
@@ -27,13 +27,13 @@ global.warncount = process.env.WARN_COUNT || 3;
 global.disablepm = process.env.DISABLE_PM || "false";
 global.disablegroup = process.env.DISABLE_GROUPS || "false",
 global.MsgsInLog = process.env.MSGS_IN_LOG || "true";
-global.userImages = process.env.USER_IMAGES || "";
-global.waPresence = process.env.WAPRESENCE || "available";
+global.userImages = process.env.USER_IMAGES || "https://telegra.ph/file/7d4468a1badba65da2628.jpg";
+global.waPresence = process.env.WAPRESENCE || "composing";
 global.readcmds = process.env.READ_COMMAND || "true";
 global.readmessage = process.env.READ_MESSAGE || "false";
 global.readmessagefrom = process.env.READ_MESSAGE_FROM || "";
 global.read_status = process.env.AUTO_READ_STATUS || "false";
-global.save_status = process.env.AUTO_SAVE_STATUS || "false";
+global.save_status = process.env.AUTO_SAVE_STATUS || "true";
 global.save_status_from = process.env.SAVE_STATUS_FROM || "all";
 global.read_status_from = process.env.READ_STATUS_FROM || "all";
 
@@ -57,7 +57,7 @@ global.SESSION_ID =
 
 
   
-  "Your session ID" //Make sure session id starts with Byte;;;
+  "Byte;;;eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoibUZISGJoTDRka0xSR2R6eENpdG1kT1d1dlN1M0lDYzR4Wmd5M1BaQXRXcz0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiZjBueG1xRHV1c3l6Q2g4TjNLSldncmpBbGl6U0hlcWxaU010VmEwUXRWWT0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiI4Q1VCOWpJYlM2TE4wOFhIc0RFRzBVNGl2MUZhcjNkbkJGdDdzTTJwZjFzPSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiI3aVRmdFJvQjVBOVQyZWR6Uk9TRU43SzRhckVtRmZPZE45RVVsbVloZmc4PSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6ImFQUWFwV1JlNEFmRHF3d2VIY1VrMGlSWFlvc1Y2NGszT21qczk1Sy9IMEk9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6Ik0xWTZFVHlHRmFhZjUwOHhNV0Q4Q25wWFBkTFE0bC9PMlF1RUlyTHJMU3c9In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiQ051OFdybHpTWGpoNnBsS1IwVnZJMlQ4ekF1bjBnQTJBLzRidkFlM3VWND0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiWW5qT0NqLzRVNkxSLzJzTStjVjRzWnFOWTkyRzRaN1IxMDhOVGhXQ0RDZz0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6ImVKQ29adkQrQytXUkdMN1F6dnNtTTJSaWVqWC9ySzlQWmtHeWYyWCtRMGE2djh6RXFaUW1yWHJaWFE2VGp2T21UWXI3ZjAra3RQVHFGVUg3S2drcGlRPT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6NTQsImFkdlNlY3JldEtleSI6Ilgwb01GWHVCbHIyV0lCVG1RUERINWY4Mm94MzFkbTRDelRpajYyWXFZL3M9IiwicHJvY2Vzc2VkSGlzdG9yeU1lc3NhZ2VzIjpbXSwibmV4dFByZUtleUlkIjozMSwiZmlyc3RVbnVwbG9hZGVkUHJlS2V5SWQiOjMxLCJhY2NvdW50U3luY0NvdW50ZXIiOjAsImFjY291bnRTZXR0aW5ncyI6eyJ1bmFyY2hpdmVDaGF0cyI6ZmFsc2V9LCJkZXZpY2VJZCI6InpBRHQ3d0ltVDRpY1hHQ2tvNlc1REEiLCJwaG9uZUlkIjoiYzUxYTM1N2YtYTMzMi00YWYwLTk2ZjItZWZhMmMzMWQwY2E1IiwiaWRlbnRpdHlJZCI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IlBxQ05TbTVwWktBYmlNN0VYRXJHS29mYURFYz0ifSwicmVnaXN0ZXJlZCI6dHJ1ZSwiYmFja3VwVG9rZW4iOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJNLzhLZkU3eGMrcGNqcHNCb1R6VWdKSk82VTA9In0sInJlZ2lzdHJhdGlvbiI6e30sInBhaXJpbmdDb2RlIjoiS0VGUVFZODEiLCJtZSI6eyJpZCI6IjIzNDgxNTIxMTk2Njg6M0BzLndoYXRzYXBwLm5ldCIsIm5hbWUiOiJCYW1pcmVpZC4ifSwiYWNjb3VudCI6eyJkZXRhaWxzIjoiQ04ycTNjZ0RFUDNJajdVR0dBSWdBQ2dBIiwiYWNjb3VudFNpZ25hdHVyZUtleSI6InVTZ0JPMlNnN000VlFmRkNaQzZmaVV6U3EyVXFRYmdGaUtPSVQ1K0xoMTg9IiwiYWNjb3VudFNpZ25hdHVyZSI6IjNiQ2h6dUJUaXVRWTBXcStpRkd3R2h6N3VjcG5sQ0lwOU5jTXdXNDVkL0thT0k3VWlsSnNHdnBCNGxDYlVzT2p1WGtaL0JjckF6TldObEpOYk1jQUFBPT0iLCJkZXZpY2VTaWduYXR1cmUiOiI3aThMakpsSmxqck1jaGRMOEU1TklWQzVNRDdYc2tKYmRkRGkvVEJROVIzSjl5L0NuZTl1OVhyUDRDY0JBQ0VLVGY1RHM5VWR3aHc5OFBzRmJ0WmRodz09In0sInNpZ25hbElkZW50aXRpZXMiOlt7ImlkZW50aWZpZXIiOnsibmFtZSI6IjIzNDgxNTIxMTk2Njg6M0BzLndoYXRzYXBwLm5ldCIsImRldmljZUlkIjowfSwiaWRlbnRpZmllcktleSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IkJia29BVHRrb096T0ZVSHhRbVF1bjRsTTBxdGxLa0c0QllpamlFK2ZpNGRmIn19XSwicGxhdGZvcm0iOiJzbWJhIiwibGFzdEFjY291bnRTeW5jVGltZXN0YW1wIjoxNzIyMDE2OTA2LCJteUFwcFN0YXRlS2V5SWQiOiJBQUFBQUwveiJ9" //Make sure session id starts with Byte;;;
 
 
 
@@ -78,13 +78,13 @@ module.exports = {
   HANDLERS: process.env.PREFIX || ".",
   BRANCH: process.env.BRANCH || "main",
   VERSION: process.env.VERSION || "1.0.0",
-  caption: process.env.CAPTION || "*Powered By TalkDrove*",
-  author: process.env.PACK_AUTHER || "TalkDrove",
-  packname: process.env.PACK_NAME || "♥️",
-  botname: process.env.BOT_NAME || "BYTE-MD",
-  ownername: process.env.OWNER_NAME || "TalkDrove",
+  caption: process.env.CAPTION || "*© Bamireid & Varry*",
+  author: process.env.PACK_AUTHER || "Varry",
+  packname: process.env.PACK_NAME || "Bamireid &",
+  botname: process.env.BOT_NAME || "Bamireid-MD",
+  ownername: process.env.OWNER_NAME || "Bamireid",
   errorChat: process.env.ERROR_CHAT || "",
-  KOYEB_API: process.env.KOYEB_API || "false",
+  KOYEB_API: process.env.KOYEB_API || "cq0w9xms22769vyl1jfkg973wwsvb0bzk9pn5rk3kd2690j67qk22l4cin35pvxm",
   REMOVE_BG_KEY: process.env.REMOVE_BG_KEY || "",
   OPENAI_API_KEY: process.env.OPENAI_API_KEY || "",
   HEROKU_API_KEY: process.env.HEROKU_API_KEY || "",
@@ -93,7 +93,7 @@ module.exports = {
   HEROKU: process.env.HEROKU_APP_NAME && process.env.HEROKU_API_KEY,
   aitts_Voice_Id: process.env.AITTS_ID || "37",
   ELEVENLAB_API_KEY: process.env.ELEVENLAB_API_KEY || "",
-  WORKTYPE: process.env.WORKTYPE || process.env.MODE || "public",
+  WORKTYPE: process.env.WORKTYPE || process.env.MODE || "private",
   LANG: (process.env.THEME || "BYTE").toUpperCase(),
 };
 global.rank = "updated";
